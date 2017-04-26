9e9a33506fb0535a5d678a9d6e3b8efa3f91a6b8
vthung <thanhhungqb@gmail.com>
2017-04-26 20:39:53 +0700

    update

--- ubuntu/basic.sh
+++ ubuntu/basic.sh
@@ -4,9 +4,11 @@
 
 apt-get update && apt-get upgrade
 
-apt install -y gcc g++ wget unzip
+apt install -y gcc g++ wget unzip curl ufw
 apt install -y supervisor
+apt-get install -y software-properties-common python-software-properties apt-transport-https
 
 ufw enable
 ufw allow 22
 
+supervisord
--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -3,8 +3,12 @@
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 apt-get update
-apt-cache policy -y docker-engine
+apt-cache policy docker-engine
 apt-get install -y docker-engine
 curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+usermod -aG docker $USER
+
+dockerd
 
