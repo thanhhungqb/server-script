02293a9318f9823e270d5b03dce80b0bc794d14d
vthung <thanhhungqb@gmail.com>
2017-12-15 05:32:04 +0700

    for newest version of Docker-CE

--- centos/docker.sh
+++ centos/docker.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 # for CentOS 7+
 
+yum -y install curl wget
 # remove old
 yum remove docker docker-common docker-selinux docker-engine > /dev/null 2>&1
 
-#yum install -y yum-utils device-mapper-persistent-data lvm2
-#yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-#yum -y install docker-ce
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce
 
 # ?
-yum -y install docker
+#yum -y install docker
 
 systemctl start docker
 systemctl status docker
