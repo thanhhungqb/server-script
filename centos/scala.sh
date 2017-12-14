57360592a8c93c2bd469d5ffe13f2e09677a5cfe
vthung <thanhhungqb@gmail.com>
2017-12-15 01:46:40 +0700

    multi version

--- centos/scala.sh
+++ centos/scala.sh
@@ -4,30 +4,36 @@ ver="2.10.7"
 ver="2.11.12"
 ver="2.12.4"
 
-for ver in '2.10.7' '2.11.12' '2.12.4'
+mkdir -p /opt/scala
+cd /opt/scala
+for ver in '2.10.7' '2.11.12' '2.12.4' '2.11.8'
 do
 url=https://downloads.lightbend.com/scala/$ver/scala-$ver.tgz
-
-cd /opt/
 wget $url
 
 tar xzf scala-$ver.tgz
 rm -rf scala-$ver.tgz
-
-
 done
+
+ver='2.9.1.final'
+url="https://scala-lang.org/files/archive/scala-2.9.1.final.tgz"
+wget $url
+tar xzf scala-$ver.tgz
+rm -rf scala-$ver.tgz
+
 
 # set default
 #ver=scala-$ver
 ver="scala-2.10.7"
 
-cd /opt/$ver
+p=/opt/scala/$ver
+cd $p
 
 # alternatives --config java
-alternatives --install /usr/bin/scala scala /opt/$ver/bin/scala 2 
-alternatives --install /usr/bin/scalac scalac /opt/$ver/bin/scalac 2
+alternatives --install /usr/bin/scala scala /$p/bin/scala 2 
+alternatives --install /usr/bin/scalac scalac /$p/bin/scalac 2
 
-alternatives --set scala /opt/$ver/bin/scala
-alternatives --set scalac /opt/$ver/bin/scalac
+alternatives --set scala /$p/bin/scala
+alternatives --set scalac /$p/bin/scalac
 
 
