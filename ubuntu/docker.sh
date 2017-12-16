35b0a7dfc42e66361c7ce8435a7b9d7a88ea0b8d
vthung <thanhhungqb@gmail.com>
2017-12-16 10:01:42 +0700

    new stype with stable version of docker on Ubuntu

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -1,14 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-get remove docker docker-engine docker.io
+
 apt-get update
-apt-cache policy docker-engine
-apt-get install -y docker-engine
-curl -L "https://github.com/docker/compose/releases/download/1.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
 
-usermod -aG docker $USER
+apt-get install -y  apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
 
-dockerd
+#usermod -aG docker $USER
 
