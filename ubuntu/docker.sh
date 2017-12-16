4b5ea53daef35cb61a81f1c9fabf5ef0d711a977
vthung <thanhhungqb@gmail.com>
2017-12-16 11:41:00 +0700

    install docker-compose

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -11,5 +11,8 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-get install -y docker-ce
 
+curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
 #usermod -aG docker $USER
 
