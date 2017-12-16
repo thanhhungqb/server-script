b51aab79d8566ce9563472df38e125835c205bfc
vthung <thanhhungqb@gmail.com>
2017-12-16 10:08:12 +0700

    add readme with use case

---
+++ ubuntu/README.sh
@@ -0,0 +1,17 @@
+# Information
+This folder contains many useful script when setup Ubuntu server
+
+Author: thanhhungqb
+
+# Use case
+
+## server with docker
+
+		#!/bin/bash
+		apt -y update && apt install -y git curl wget
+		cd /opt && git clone https://github.com/thanhhungqb/server-script
+		/opt/server-script/ubuntu/docker.sh
+
+
+
+
