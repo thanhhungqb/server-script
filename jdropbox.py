273e5866fa64cb5bda1fd108531678dfd8050d3a
vthung <thanhhungqb@gmail.com>
2018-02-07 19:10:57 +0700

    add upload function

--- jdropbox.py
+++ jdropbox.py
@@ -2,6 +2,8 @@
 
 import os
 import argparse
+import datetime
+import time
 
 import dropbox
 from dropbox.files import FileMetadata, FolderMetadata
@@ -30,21 +32,54 @@ def download_folder(dbx,src_folder,dest_folder=None):
 		download_folder(dbx, fd, ndest_folder)
 
 
-		
+
+def upload(dbx, fullname, folder, subfolder, name, overwrite=True):
+    """Upload a file.
+    Return the request response, or None in case of error.
+    """
+    path = '/%s/%s/%s' % (folder, subfolder.replace(os.path.sep, '/'), name)
+    while '//' in path:
+        path = path.replace('//', '/')
+    mode = (dropbox.files.WriteMode.overwrite
+            if overwrite
+            else dropbox.files.WriteMode.add)
+    mtime = os.path.getmtime(fullname)
+    with open(fullname, 'rb') as f:
+        data = f.read()
+    #with stopwatch('upload %d bytes' % len(data)):
+    if True:
+        try:
+            res = dbx.files_upload(
+                data, path, mode,
+                client_modified=datetime.datetime(*time.gmtime(mtime)[:6]),
+                mute=True)
+        except dropbox.exceptions.ApiError as err:
+            print('*** API error', err)
+            return None
+    print('uploaded as', res.name.encode('utf8'))
+    return res
+    
+    		
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--token', help='token')
 	parser.add_argument('--src', help='source foler on dropbox apps. Ex: /test')
 	parser.add_argument('--dest', help='dest in machine folder. Ex: /tmp')
+	parser.add_argument('--upload', action='store_true', help='if download/upload')
 		
 	args = parser.parse_args()
 	
 	dbx = dropbox.Dropbox(args.token)
-	download_folder(dbx, args.src, args.dest)
+	if not args.upload:
+		download_folder(dbx, args.src, args.dest)
+	else:
+		n = args.src.split('/')[-1]
+		print(upload(dbx, args.src, '/', args.dest, n))
 	
 	
 if __name__ == '__main__':
 	main()
 	
 # requires: pip install dropbox
+# python t.py --token $TOKEN --upload --src t.txt --dest bk
 
